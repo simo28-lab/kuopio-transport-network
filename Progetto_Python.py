3273f465dc0844f7796e8cdf4f41fa13c716c252
simo28-lab <simonamagnani28@gmail.com>
2025-10-05 15:44:42 +0200

    Update Progetto_Python.py

--- Progetto_Python.py
+++ Progetto_Python.py
@@ -150,7 +150,6 @@ for u, v in G_week.edges():
 
 
 #print the top 10
-print("--- "Number of times each stop appears as a destination node (daily and weekly)" ---")
 print("Top 10 stops by number of arrivals:")
 
 top_10_day=sorted(diz_arrivi_day.items(), key=lambda x: x[1], reverse=True )[:10] 
@@ -401,6 +400,7 @@ print('the number of unique direct connections weekly is:', num_unique_direct_co
 print('the execution weekly time is:', f'{execution_time_week:.16f}')
 
    
+
 
 
 
